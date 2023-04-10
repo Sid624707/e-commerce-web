23fc19745ffabcd4d44f2f14e61159e29557020e
Sid624707 <sudharshan_thammaiahgari@epam.com>
2023-04-10 23:47:37 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Cart from './features/cart/Cart';
+import { Route,Routes } from 'react-router-dom';
+import DisplayBooks from './features/book/DisplayBooks';
+import SelectedBook from './features/bookDetail/SelectedBook';
+import MyOrders from './features/order/MyOrders';
+import NoMatch from './components/NoMatch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<DisplayBooks />}/>
+        <Route path='/:selectedBook/:id' element={<SelectedBook />}/>
+        <Route path='/myOrders' element={<MyOrders />}/>
+        <Route path='/cart' element={<Cart />}/>
+        <Route path='*' element={<NoMatch />}/>
+      </Routes>
+    </>
+  
   );
 }
 
